37cf9ed528285e22a53110ad24fd13a81aee393a
Sergey Kacheev <s.kacheev@gmail.com>
2021-09-04 15:28:28 +0700

    Antialiasing

---
+++ src/camera.rs
@@ -0,0 +1,38 @@
+use super::ray::Ray;
+use super::vec::{Point3, Vec3};
+
+pub struct Camera {
+    origin: Point3,
+    lower_left_corner: Point3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        const ASPECT_RATIO: f64 = 16.0 / 9.0;
+        const VIEWPORT_HEIGHT: f64 = 2.0;
+        const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+        const FOCAL_LENGTH: f64 = 1.0;
+
+        let origin = Point3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
+        Camera {
+            origin,
+            horizontal,
+            vertical,
+            lower_left_corner,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical,
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+mod camera;
 mod hit;
 mod ray;
 mod sphere;
 mod vec;
 
+use camera::Camera;
 use hit::{Hit, World};
+use rand::{thread_rng, Rng};
 use ray::Ray;
 use sphere::Sphere;
-use vec::{Color, Point3, Vec3};
+use vec::{Color, Point3};
 
 fn ray_color(r: &Ray, world: &World) -> Color {
     if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
@@ -23,6 +26,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIGHT: u64 = 400;
     const IMAGE_HEIGHT: u64 = (IMAGE_WIGHT as f64 / ASPECT_RATIO) as u64;
+    const SAMPLES_PER_PIXEL: u64 = 100;
 
     // World
     let mut world = World::new();
@@ -30,30 +34,29 @@ fn main() {
     world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_lenght = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_lenght);
+    let cam = Camera::new();
 
     println!("P3");
     println!("{} {}", IMAGE_WIGHT, IMAGE_HEIGHT);
     println!("255");
 
+    let mut rng = thread_rng();
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining: {}", j);
 
         for i in 0..IMAGE_WIGHT {
-            let u = (i as f64) / ((IMAGE_WIGHT - 1) as f64);
-            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let random_u: f64 = rng.gen();
+                let random_v: f64 = rng.gen();
 
-            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            let pixel_color = ray_color(&r, &world);
-            println!("{}", pixel_color.format_color());
+                let u = ((i as f64) + random_u) / ((IMAGE_WIGHT - 1) as f64);
+                let v = ((j as f64) + random_v) / ((IMAGE_HEIGHT - 1) as f64);
+
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
         }
     }
     eprintln!("Done.")
